b90caebf9bc72da5e1519c5122b5322fab3b13e6
RepolloDev <96484170+RepolloDev@users.noreply.github.com>
2025-11-26 08:33:08 -0400

    Cambio de contenedor de Oracle a PostgreSQL y automatizacion de creacion de tablas y datos

--- docker/scripts/init-db.sh
+++
@@ -1,5 +0,0 @@
-echo "INIT: ejecutando schema.sql"
-sqlplus -s myuser/mypassword@//localhost/XE @/opt/oracle/seed/schema.sql
-echo "INIT: ejecutando values.sql"
-sqlplus -s myuser/mypassword@//localhost/XE @/opt/oracle/seed/values.sql
-echo "INIT: finalizado"
